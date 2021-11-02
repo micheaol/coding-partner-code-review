1a3245d5de1676e2ad08295e53b483caf4782b67
micheaol <micheaol80@gmail.com>
2021-11-02 01:11:28 +0100

    fix: remove repeated code from the codebase

--- src/tasks.js
+++ src/tasks.js
@@ -16,7 +16,6 @@ const enterTasks = getMyElement('#enter-tasks');
 const enterIcon = getMyElement('#enter-icon');
 
 function getUserData() {
-  // e.preventDefault();
   const newTask = new UserTask(enterTasks.value);
   if (enterTasks.value.length === 0) {
     enterTasks.placeholder = 'Your task can not be EMPTY......';
